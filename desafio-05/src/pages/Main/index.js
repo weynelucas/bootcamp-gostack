547f8d1239b69e77b49176b589151cd59b2e5151
Lucas Weyne <weynelucas@gmail.com>
2019-08-31 09:40:33 -0300

    Implementando submit do form

--- desafio-05/src/pages/Main/index.js
+++ desafio-05/src/pages/Main/index.js
@@ -4,21 +4,46 @@ import { GoMarkGithub, GoRepo } from 'react-icons/go';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <GoMarkGithub />
-        Repositories
-      </h1>
+export default class Main extends React.Component {
+  state = {
+    repositories: [],
+    newRepo: '',
+  };
 
-      <Form>
-        <input type="text" placeholder="Find a repository..." />
-        <SubmitButton>
-          <GoRepo size={16} />
-          New
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      repositories: [...this.state.repositories],
+      newRepo: '',
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <h1>
+          <GoMarkGithub />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Find a repository..."
+            onChange={this.handleInputChange}
+            value={this.state.newRepo}
+          />
+          <SubmitButton>
+            <GoRepo size={16} />
+            New
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
