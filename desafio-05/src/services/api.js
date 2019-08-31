da3679be642e748152924878b901bc4d26a91822
Lucas Weyne <weynelucas@gmail.com>
2019-08-31 14:43:33 -0300

    Parametrizando autenticação na API

--- desafio-05/src/services/api.js
+++ desafio-05/src/services/api.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://api.github.com',
+  auth: {
+    username: process.env.REACT_APP_API_USER,
+    password: process.env.REACT_APP_API_PASS,
+  },
 });
 
 export default api;
