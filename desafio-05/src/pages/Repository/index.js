5a9ee3e493603971f154c9357a14f56493a87c56
Lucas Weyne <weynelucas@gmail.com>
2019-08-31 13:52:39 -0300

    Recuperando repo e issues na rota repository

--- desafio-05/src/pages/Repository/index.js
+++ desafio-05/src/pages/Repository/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
+import api from '../../services/api';
+
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends React.Component {
+  state = {
+    repository: {},
+    issues: [],
+  };
+
+  async componentDidMount() {
+    const repoName = decodeURIComponent(this.props.match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`repos/${repoName}`),
+      api.get(`repos/${repoName}/issues`, {
+        params: {
+          per_page: 5,
+          state: 'open',
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+    });
+  }
+
+  render() {
+    const { repository, issues } = this.state;
+
+    return <h1>Repository: {decodeURIComponent(repository.name)}</h1>;
+  }
 }
