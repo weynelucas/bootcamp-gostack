97eaa598e90ad22f9fc8a167ba8fca595370db00
Lucas Weyne <weynelucas@gmail.com>
2019-09-03 00:00:49 -0300

    Estilo formulário

--- desafio-06/src/pages/Main/styles.js
+++ desafio-06/src/pages/Main/styles.js
@@ -1,6 +1,31 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  border-bottom-width: 1px;
+  padding-bottom: 20px;
+  border-bottom-color: #eee;
+`;
+
+export const Input = styled.TextInput`
+  flex: 1;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #eee;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  align-items: center;
+  justify-content: center;
+  background-color: #2cbe4e;
+  padding: 0 15px;
+  margin-left: 15px;
 `;
