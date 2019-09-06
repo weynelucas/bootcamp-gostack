94d098ea8e3d20b1b3011dca324540b1ab3e4145
Lucas Weyne <weynelucas@gmail.com>
2019-09-06 07:22:13 -0300

    Estilização home

--- desafio-07/web/src/pages/Home/index.js
+++ desafio-07/web/src/pages/Home/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_detalhe2.jpg?ims=326x"
+          alt="Tênis Nike Revolution"
+        />
+        <strong>Tênis Nike Revolution</strong>
+        <span>R$ 159,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
