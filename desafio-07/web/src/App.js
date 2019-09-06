d10dafc886a5b697eba0a8bc0c2594e8188b5e3f
Lucas Weyne <weynelucas@gmail.com>
2019-09-05 22:53:19 -0300

    Configurando rotas

--- desafio-07/web/src/App.js
+++ desafio-07/web/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import Header from './components/Header';
 
 function App() {
-  return <div />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
