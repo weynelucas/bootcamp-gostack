0cc2732e827f4641c8f413bcd65bd983bab59354
Lucas Weyne <weynelucas@gmail.com>
2019-09-06 07:04:04 -0300

    Header

--- desafio-07/web/src/components/Header/index.js
+++ desafio-07/web/src/components/Header/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/images/logo.svg';
 
 export default function Header() {
-  return <div />;
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 itens</span>
+        </div>
+        <MdShoppingBasket color="#fff" size={36} />
+      </Cart>
+    </Container>
+  );
 }
