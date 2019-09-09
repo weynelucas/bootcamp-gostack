941edf39ef4153838c2ddb74062210dc2fbe2b3a
Lucas Weyne <weynelucas@gmail.com>
2019-09-09 12:58:53 -0300

    Configurando reducers

--- desafio-07/web/src/App.js
+++ desafio-07/web/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
+import store from './store';
 
 import Routes from './routes';
 import Header from './components/Header';
@@ -7,11 +10,13 @@ import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
