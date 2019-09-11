8f1ec88bc9218959a85c7b75aeece306ecfdec69
Lucas Weyne <weynelucas@gmail.com>
2019-09-11 09:31:12 -0300

    Add react-native-vector-icons

--- desafio-08/src/components/Header/index.js
+++ desafio-08/src/components/Header/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import {Container, Logo} from './styles';
 
 export default function Header() {
   return (
     <Container>
       <Logo source={logo} />
+      <Icon name="shopping-basket" color="#fff" size={26} />
     </Container>
   );
 }
