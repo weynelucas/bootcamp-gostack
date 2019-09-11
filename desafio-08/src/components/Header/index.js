1aa926fc8312e481e4977e66d49a46dd03aa0e5f
Lucas Weyne <weynelucas@gmail.com>
2019-09-11 09:42:11 -0300

    CartAmount

--- desafio-08/src/components/Header/index.js
+++ desafio-08/src/components/Header/index.js
@@ -3,13 +3,17 @@ import logo from '../../assets/logo.png';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container, Logo} from './styles';
+import {Container, Logo, Cart, CartAmount} from './styles';
 
 export default function Header() {
   return (
     <Container>
       <Logo source={logo} />
-      <Icon name="shopping-basket" color="#fff" size={26} />
+
+      <Cart>
+        <Icon name="shopping-basket" color="#fff" size={26} />
+        <CartAmount>3</CartAmount>
+      </Cart>
     </Container>
   );
 }
--- desafio-08/src/components/Header/styles.js
+++ desafio-08/src/components/Header/styles.js
@@ -11,3 +11,20 @@ export const Logo = styled.Image`
   width: 185px;
   height: 24px;
 `;
+
+export const Cart = styled.View`
+  position: relative;
+`;
+
+export const CartAmount = styled.Text`
+  position: absolute;
+  top: 1;
+  right: -5;
+  font-size: 12px;
+  height: 16px;
+  width: 16px;
+  border-radius: 8px;
+  color: #fff;
+  text-align: center;
+  background-color: #7159c1;
+`;
