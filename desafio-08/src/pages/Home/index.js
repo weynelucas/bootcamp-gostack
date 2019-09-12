4fb268538a25014c5102a40294cbc805a684b57d
Lucas Weyne <weynelucas@gmail.com>
2019-09-11 22:32:30 -0300

    Estilo Home

--- desafio-08/src/pages/Home/index.js
+++ desafio-08/src/pages/Home/index.js
@@ -1,8 +1,56 @@
 import React from 'react';
-import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import colors from '../../styles/colors';
+import {
+  AddToCartButton,
+  AddToCartButtonText,
+  Container,
+  ProductAmount,
+  ProductAmountText,
+  ProductImage,
+  ProductItem,
+  ProductList,
+  ProductPrice,
+  ProductTitle,
+} from './styles';
 
-// import { Container } from './styles';
-
-export default function Home() {
-  return <View />;
+export default function Home({navigation}) {
+  return (
+    <Container>
+      <ProductList
+        data={[
+          {
+            id: 1,
+            title: 'Tênis de Caminhada Leve Confortável',
+            price: 179.9,
+            image:
+              'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+          },
+          {
+            id: 2,
+            title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+            price: 139.9,
+            image:
+              'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+          },
+        ]}
+        horizontal={true}
+        keyExtractor={item => String(item.id)}
+        renderItem={({item}) => (
+          <ProductItem>
+            <ProductImage source={{uri: item.image}} />
+            <ProductTitle>{item.title}</ProductTitle>
+            <ProductPrice>{item.price}</ProductPrice>
+            <AddToCartButton>
+              <ProductAmount>
+                <Icon name="add-shopping-cart" size={16} color={colors.white} />
+                <ProductAmountText>3</ProductAmountText>
+              </ProductAmount>
+              <AddToCartButtonText>ADICIONAR</AddToCartButtonText>
+            </AddToCartButton>
+          </ProductItem>
+        )}
+      />
+    </Container>
+  );
 }
---
+++ desafio-08/src/pages/Home/styles.js
@@ -0,0 +1,70 @@
+import {rgba} from 'polished';
+import {FlatList} from 'react-native';
+import {RectButton} from 'react-native-gesture-handler';
+import styled from 'styled-components/native';
+import colors from '../../styles/colors';
+
+export const Container = styled.View`
+  background: ${colors.lightDark};
+  flex: 1;
+  padding: 20px 0;
+`;
+
+export const ProductList = styled(FlatList)``;
+
+export const ProductItem = styled.View`
+  background: ${colors.white};
+  border-radius: 4px;
+  padding: 10px;
+  margin: 0 10px;
+  width: 220px;
+  height: 358px;
+`;
+
+export const ProductImage = styled.Image`
+  width: 200px;
+  height: 200px;
+`;
+
+export const ProductTitle = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  font-size: 16px;
+  line-height: 21px;
+  color: #333333;
+`;
+
+export const ProductPrice = styled.Text`
+  margin-top: 5px;
+  font-size: 21px;
+  font-weight: bold;
+`;
+
+export const AddToCartButton = styled(RectButton)`
+  margin-top: auto;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 4px;
+  background-color: ${colors.primary};
+`;
+
+export const ProductAmount = styled.View`
+  padding: 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: ${rgba(0, 0, 0, 0.1)};
+`;
+
+export const ProductAmountText = styled.Text`
+  font-size: 14px;
+  margin-left: 5px;
+  color: ${colors.white};
+`;
+
+export const AddToCartButtonText = styled.Text`
+  font-size: 14px;
+  color: ${colors.white};
+  flex: 1;
+  text-align: center;
+`;
