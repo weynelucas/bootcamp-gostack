664b7b26f49d60a198f4fa8853f38279f4532371
Lucas Weyne <weynelucas@gmail.com>
2019-09-12 07:20:50 -0300

    Estrutura da tela

--- desafio-08/src/pages/Cart/index.js
+++ desafio-08/src/pages/Cart/index.js
@@ -1,8 +1,101 @@
 import React from 'react';
-import {View} from 'react-native';
-
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import colors from '../../styles/colors';
+import {
+  CartContainer,
+  CartFooter,
+  CartItem,
+  CartItemActionButton,
+  CartItemActions,
+  CartItemAmount,
+  CartItemBody,
+  CartItemDeleteButton,
+  CartItemFooter,
+  CartItemImage,
+  CartItemInfo,
+  CartItemPrice,
+  CartItemSubtotal,
+  CartItemTitle,
+  CartList,
+  CartTotal,
+  CartTotalText,
+  CheckoutButton,
+  CheckoutButtonText,
+  Container,
+} from './styles';
 
 export default function Cart() {
-  return <View />;
+  return (
+    <Container>
+      <CartContainer>
+        <CartList
+          data={[
+            {
+              id: 1,
+              title: 'Tênis de Caminhada Leve Confortável',
+              price: 179.9,
+              priceFormatted: 'R$ 179,90',
+              amount: 3,
+              subtotal: 'R$ 539,70',
+              image:
+                'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+            },
+            {
+              id: 2,
+              title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+              price: 139.9,
+              priceFormatted: 'R$ 139,90',
+              subtotal: 'R$ 139,90',
+              image:
+                'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+            },
+          ]}
+          renderItem={({item}) => (
+            <CartItem>
+              <CartItemBody>
+                <CartItemImage source={{uri: item.image}} />
+                <CartItemInfo>
+                  <CartItemTitle>{item.title}</CartItemTitle>
+                  <CartItemPrice>{item.priceFormatted}</CartItemPrice>
+                </CartItemInfo>
+                <CartItemDeleteButton>
+                  <Icon name="delete-forever" size={24} color={colors.white} />
+                </CartItemDeleteButton>
+              </CartItemBody>
+              <CartItemFooter>
+                <CartItemActions>
+                  <CartItemActionButton>
+                    <Icon
+                      name="remove-circle-outline"
+                      size={20}
+                      color={colors.white}
+                    />
+                  </CartItemActionButton>
+
+                  <CartItemAmount value={item.amount} keyboardType="numeric" />
+
+                  <CartItemActionButton>
+                    <Icon
+                      name="add-circle-outline"
+                      size={20}
+                      color={colors.white}
+                    />
+                  </CartItemActionButton>
+                </CartItemActions>
+
+                <CartItemSubtotal>{item.subtotal}</CartItemSubtotal>
+              </CartItemFooter>
+            </CartItem>
+          )}
+        />
+        <CartFooter>
+          <CartTotalText>TOTAL</CartTotalText>
+          <CartTotal>R$ 1619,10</CartTotal>
+          <CheckoutButton>
+            <CheckoutButtonText>FINALIZAR PEDIDO</CheckoutButtonText>
+          </CheckoutButton>
+        </CartFooter>
+      </CartContainer>
+    </Container>
+  );
 }
---
+++ desafio-08/src/pages/Cart/styles.js
@@ -0,0 +1,43 @@
+import {FlatList} from 'react-native';
+import {BorderlessButton, RectButton} from 'react-native-gesture-handler';
+import styled from 'styled-components/native';
+
+export const CartContainer = styled.View``;
+
+export const CartItem = styled.View``;
+
+export const CartItemActions = styled.View``;
+
+export const CartItemActionButton = styled(BorderlessButton)``;
+
+export const CartItemAmount = styled.TextInput``;
+
+export const CartItemBody = styled.View``;
+
+export const CartItemDeleteButton = styled(BorderlessButton)``;
+
+export const CartItemFooter = styled.View``;
+
+export const CartItemImage = styled.Image``;
+
+export const CartItemInfo = styled.View``;
+
+export const CartItemPrice = styled.Text``;
+
+export const CartItemTitle = styled.Text``;
+
+export const CartList = styled(FlatList)``;
+
+export const CartTotal = styled.Text``;
+
+export const CartTotalText = styled.Text``;
+
+export const CheckoutButton = styled(RectButton)``;
+
+export const CheckoutButtonText = styled.Text``;
+
+export const CartFooter = styled.View``;
+
+export const CartItemSubtotal = styled.Text``;
+
+export const Container = styled.View``;
